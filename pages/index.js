9b87295753bb9a6b78498ac5de22d5972cf049f8
Kamal040102 <kamal5201ks@gmail.com>
2022-04-15 20:18:43 +0530

    SEO v0.0.3

--- pages/index.js
+++ pages/index.js
@@ -10,17 +10,7 @@ export default function Home() {
       <Head>
         <title>Home | Kamal Sharma</title>
         <meta name="description" content="I am a third year under graduate, Computer Engineering student and Data Science Enthusiast. I have worked as an intern in various companies as a Data Science Trainee, also I am an intermediate Full-Stack Web Developer and learning daily to improve my self. Currently I am focusing more towards learning the fundamentals of Data Science and creating my own Metaverse." />
-        <meta name="description" content="kamal" />
-        <meta name="description" content="science" />
-        <meta name="description" content="sharma" />
-        <meta name="description" content="resume" />
-        <meta name="description" content="portfolio" />
-        <meta name="description" content="projects" />
-        <meta name="description" content="data science" />
-        <meta name="description" content="Portfolio Web" />
-        <meta name="description" content="Data Science Portfolio Web" />
-        <meta name="description" content="Full Stack Developer Portfolio Web" />
-        <meta name="description" content="Next JS, Bootstrap, Tailwind css Portfolio Web" />
+        <meta name="keywords" content="kamal, sharma, science, resume, portfolio, projects, data science, web development, full stack development, Data Scientist Portfolio Web, Full Stack Developer Portfolio Web, Next JS, Bootstrap, Tailwind css Portfolio Web, Portfolio Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div
