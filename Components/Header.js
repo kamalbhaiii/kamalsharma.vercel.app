22f090f26c49dcb51abd72b6803225b6f023a22f
Kamal040102 <kamal5201ks@gmail.com>
2022-04-15 12:12:51 +0530

    Minor Changes 15-04-22

--- Components/Header.js
+++ Components/Header.js
@@ -8,19 +8,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Link from 'next/link'
 // import 'react-bootstrap'
 // import Script from 'next/script'
-// import styles from '../styles/Header.module.css'
+import styles from '../styles/Header.module.css'
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark" fixed="top" expand="lg">
+    <Navbar className={styles.navBg} variant="dark" fixed="top" expand="lg">
   <Container>
-    <Link href="/"><Navbar.Brand>Kamal Sharma</Navbar.Brand></Link>
+    <Link href="/"><Navbar.Brand className={styles.cursorPointer}>Kamal Sharma</Navbar.Brand></Link>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ms-auto">
-        <Link href="/#AboutMe"><Navbar.Brand>About Me</Navbar.Brand></Link>
-        <Link href="/Resume"><Navbar.Brand>Resume</Navbar.Brand></Link>
-        <Link href="/Projects"><Navbar.Brand>Projects</Navbar.Brand></Link>
+        <Link href="/#AboutMe"><Navbar.Brand className={styles.cursorPointer}>About Me</Navbar.Brand></Link>
+        <Link href="/Resume"><Navbar.Brand className={styles.cursorPointer}>Resume</Navbar.Brand></Link>
+        <Link href="/Projects"><Navbar.Brand className={styles.cursorPointer}>Projects</Navbar.Brand></Link>
       </Nav>
     </Navbar.Collapse>
   </Container>
